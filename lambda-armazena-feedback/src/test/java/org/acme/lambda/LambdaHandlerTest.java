8d5663022e1f610b5a3b54b1590b18d582e630bc
Guilherme Gaspar <guilherme.c.gaspargg@gmail.com>
2026-05-27 23:05:35 -0300

    Remove implementacao de persistencia da lambda-armazena-feedback

--- lambda-armazena-feedback/src/test/java/org/acme/lambda/LambdaHandlerTest.java
+++ lambda-armazena-feedback/src/test/java/org/acme/lambda/LambdaHandlerTest.java
@@ -1,25 +1,30 @@
 package org.acme.lambda;
 
-import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
-import java.util.Map;
+import io.quarkus.test.junit.QuarkusTest;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.isOneOf;
+import static org.hamcrest.CoreMatchers.containsString;
 
 @QuarkusTest
 class LambdaHandlerTest {
-
     @Test
-    void shouldInvokeLambdaHandler() {
+    void testSimpleLambdaSuccess() throws Exception {
+        // you test your lambdas by invoking on http://localhost:8081
+        // this works in dev mode too
+
+        Person in = new Person();
+        in.setName("Stu");
         given()
                 .contentType("application/json")
                 .accept("application/json")
-                .body(Map.of("descricao", "Aula boa", "nota", 8, "urgency", "MEDIA"))
+                .body(in)
                 .when()
                 .post()
                 .then()
-                .statusCode(isOneOf(200, 500));
+                .statusCode(200)
+                .body(containsString("Hello Stu"));
     }
+
 }
